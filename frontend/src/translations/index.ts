887551d1fc60c908a72ec0b86761a8d3a0806d7a
et-nik <nikita.hldm@gmail.com>
2026-09-17 02:48:06 +0200

    de and es langs

--- frontend/src/translations/index.ts
+++ frontend/src/translations/index.ts
@@ -1,5 +1,7 @@
+import { de } from './de';
 import { en } from './en';
+import { es } from './es';
 import { ru } from './ru';
 
-export const translations = { en, ru };
-export { en, ru };
+export const translations = { en, ru, de, es };
+export { de, en, es, ru };
